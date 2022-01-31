251667ab71465bc9fa88be6249480628bc06ea97
pkm <pmahto692@gmail.com>
2022-01-31 11:21:16 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, {Component} from "react";
+import Button from '@mui/material/Button';
 
 class App extends Component {
     render(){
         return (
-            <h1>Hello Praksh</h1>
+            <>
+             <h1>Hello prakash</h1>
+            <Button>Click Me</Button>
+            <Button href="https://www.pkminfotech.com" color="secondary">Go to pkm infotech</Button>
+            <Button disabled > Hide</Button>
+            <Button variant="contained" >Back </Button>
+
+            </>
+           
         )
 
     }
